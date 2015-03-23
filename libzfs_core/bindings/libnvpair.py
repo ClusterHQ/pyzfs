f19243497fbb040c14707a1d994f5c4617adfe55
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-03-23 18:16:28 +0200

    clean up nvpair/nvlist bindings
    we only use a limited subset of the api in our implementation
    of libzfs_core wrapper

--- libzfs_core/bindings/libnvpair.py
+++ libzfs_core/bindings/libnvpair.py
@@ -3,11 +3,12 @@ CDEF = """
 	typedef ... nvpair_t;
 	enum data_type_t { ... };
 	typedef bool boolean_t;
+	typedef unsigned char uchar_t;
+	typedef unsigned int uint_t;
 
 	int nvlist_alloc(nvlist_t **, uint_t, int);
 	void nvlist_free(nvlist_t *);
 
-	int nvlist_add_nvpair(nvlist_t *, nvpair_t *);
 	int nvlist_add_boolean(nvlist_t *, const char *);
 	int nvlist_add_boolean_value(nvlist_t *, const char *, boolean_t);
 	int nvlist_add_byte(nvlist_t *, const char *, uchar_t);
@@ -34,41 +35,6 @@ CDEF = """
 	int nvlist_add_string_array(nvlist_t *, const char *, char *const *, uint_t);
 	int nvlist_add_nvlist_array(nvlist_t *, const char *, nvlist_t **, uint_t);
 
-	int nvlist_lookup_boolean(nvlist_t *, const char *);
-	int nvlist_lookup_boolean_value(nvlist_t *, const char *, boolean_t *);
-	int nvlist_lookup_byte(nvlist_t *, const char *, uchar_t *);
-	int nvlist_lookup_int8(nvlist_t *, const char *, int8_t *);
-	int nvlist_lookup_uint8(nvlist_t *, const char *, uint8_t *);
-	int nvlist_lookup_int16(nvlist_t *, const char *, int16_t *);
-	int nvlist_lookup_uint16(nvlist_t *, const char *, uint16_t *);
-	int nvlist_lookup_int32(nvlist_t *, const char *, int32_t *);
-	int nvlist_lookup_uint32(nvlist_t *, const char *, uint32_t *);
-	int nvlist_lookup_int64(nvlist_t *, const char *, int64_t *);
-	int nvlist_lookup_uint64(nvlist_t *, const char *, uint64_t *);
-	int nvlist_lookup_string(nvlist_t *, const char *, char **);
-	int nvlist_lookup_nvlist(nvlist_t *, const char *, nvlist_t **);
-	int nvlist_lookup_boolean_array(nvlist_t *, const char *,
-	    boolean_t **, uint_t *);
-	int nvlist_lookup_byte_array(nvlist_t *, const char *, uchar_t **, uint_t *);
-	int nvlist_lookup_int8_array(nvlist_t *, const char *, int8_t **, uint_t *);
-	int nvlist_lookup_uint8_array(nvlist_t *, const char *, uint8_t **, uint_t *);
-	int nvlist_lookup_int16_array(nvlist_t *, const char *, int16_t **, uint_t *);
-	int nvlist_lookup_uint16_array(nvlist_t *, const char *, uint16_t **, uint_t *);
-	int nvlist_lookup_int32_array(nvlist_t *, const char *, int32_t **, uint_t *);
-	int nvlist_lookup_uint32_array(nvlist_t *, const char *, uint32_t **, uint_t *);
-	int nvlist_lookup_int64_array(nvlist_t *, const char *, int64_t **, uint_t *);
-	int nvlist_lookup_uint64_array(nvlist_t *, const char *, uint64_t **, uint_t *);
-	int nvlist_lookup_string_array(nvlist_t *, const char *, char ***, uint_t *);
-	int nvlist_lookup_nvlist_array(nvlist_t *, const char *,
-	    nvlist_t ***, uint_t *);
-
-	int nvlist_lookup_nvpair(nvlist_t *, const char *, nvpair_t **);
-	int nvlist_lookup_nvpair_embedded_index(nvlist_t *, const char *, nvpair_t **,
-	    int *, char **);
-	boolean_t nvlist_exists(nvlist_t *, const char *);
-	boolean_t nvlist_empty(nvlist_t *);
-
-	/* processing nvpair */
 	nvpair_t *nvlist_next_nvpair(nvlist_t *, nvpair_t *);
 	nvpair_t *nvlist_prev_nvpair(nvlist_t *, nvpair_t *);
 	char *nvpair_name(nvpair_t *);
