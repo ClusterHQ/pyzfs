65308a0fc1519523acfb1df4c5655013027d48b6
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-03-30 12:56:21 +0300

    cosmetic changes in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name = "pyzfs",
     version = "0.1",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    include_package_data=True,
+    packages = find_packages(exclude = ["tests", "test", "tests.*"]),
+#   packages = ['libzfs_core', 'libzfs_core/bindings'],
+    include_package_data = True,
     install_requires = [
         "cffi",
     ],
