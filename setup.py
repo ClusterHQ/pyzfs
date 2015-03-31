6e2b601d04e173b98ba4fda55e979d57bfd4f1cd
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-03-31 17:51:00 +0300

    we typically do not exclude tests from a package

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "pyzfs",
     version = "0.1",
-    packages = find_packages(exclude = ["tests", "test", "tests.*"]),
-#   packages = ['libzfs_core', 'libzfs_core/bindings'],
+    packages = find_packages(),
     include_package_data = True,
     install_requires = [
         "cffi",
