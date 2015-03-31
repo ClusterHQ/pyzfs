15bef50a422a4a9facf15d3e0518f94e266e710f
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-03-31 14:39:22 +0300

    ctypes: add stricter enum value checking to _ffi_cast
    It seems that neither ffi.new nor ffi.cast enforce that an interger
    initializer can be mapped to a valid (defined) enum value.

--- libzfs_core/ctypes.py
+++ libzfs_core/ctypes.py
@@ -6,9 +6,17 @@ from .bindings.libnvpair import ffi as _ffi
 
 
 def _ffi_cast(type_name):
+    type_info = _ffi.typeof(type_name)
     def _func(value):
         # this is for overflow / underflow checking only
-        _ffi.new(type_name + '*', value)
+        if type_info.kind == 'enum':
+            try:
+                type_info.elements[value]
+            except KeyError as e:
+                raise OverflowError('Invalid enum <%s> value %s' %
+                    (type_info.cname, e.message))
+        else:
+            _ffi.new(type_name + '*', value)
         return _ffi.cast(type_name, value)
     _func.__name__ = type_name
     return _func
