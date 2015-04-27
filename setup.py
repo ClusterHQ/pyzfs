fd499a7433d947f6b3308500ae3af1054346c374
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-04-27 13:32:12 +0300

    setup: add more package information

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from setuptools import setup, find_packages
 setup(
     name = "pyzfs",
     version = "0.1",
+    description = "Wrapper for libzfs_core",
+    author="ClusterHQ",
+    author_email="support@clusterhq.com",
+    url="https://clusterhq.com/",
+    license="Apache License, Version 2.0",
+
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
