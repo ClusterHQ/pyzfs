90fa53d70068acb98f1d72e6e9ee551896c443ca
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-07-16 14:47:46 +0300

    setup: consistently use spaces around keyword assignment operators

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name = "pyzfs",
     version = "0.1",
     description = "Wrapper for libzfs_core",
-    author="ClusterHQ",
-    author_email="support@clusterhq.com",
-    url="https://clusterhq.com/",
-    license="Apache License, Version 2.0",
+    author = "ClusterHQ",
+    author_email = "support@clusterhq.com",
+    url = "https://clusterhq.com/",
+    license = "Apache License, Version 2.0",
 
     packages = find_packages(),
     include_package_data = True,
@@ -20,7 +20,7 @@ setup(
         "cffi",
     ],
     zip_safe = False,
-    test_suite="libzfs_core.test",
+    test_suite = "libzfs_core.test",
 )
 
 # vim: softtabstop=4 tabstop=4 expandtab shiftwidth=4
