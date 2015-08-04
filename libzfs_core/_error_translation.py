9e31d45d0917c3f911ce6d06370a1874436906a6
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-08-04 16:11:19 +0300

    add docstring to _handle_err_list()
    Also, document N_MORE_ERRORS special element of the error list.

--- libzfs_core/_error_translation.py
+++ libzfs_core/_error_translation.py
@@ -399,6 +399,33 @@ def lzc_receive_xlate_error(ret, snapname, fd, force, origin, props):
 
 
 def _handle_err_list(ret, errlist, names, exception, mapper):
+    '''
+    Convert one or more errors from an operation into the requested exception.
+
+    :param int ret: the overall return code.
+    :param errlist: the dictionary that maps entity names to their specific error codes.
+    :type errlist: dict of bytes:int
+    :param names: the list of all names of the entities on which the operation was attempted.
+    :param type exception: the type of the exception to raise if an error occurred.
+                           The exception should be a subclass of `MultipleOperationsFailure`.
+    :param function mapper: the function that maps an error code and a name to a Python exception.
+
+    Unless ``ret`` is zero this function will raise the ``exception``.
+    If the ``errlist`` is not empty, then the compound exception will contain a list of exceptions
+    corresponding to each individual error code in the ``errlist``.
+    Otherwise, the ``exception`` will contain a list with a single exception corresponding to the
+    ``ret`` value.  If the ``names`` list contains only one element, that is, the operation was
+    attempted on a single entity, then the name of that entity is passed to the ``mapper``.
+    If the operation was attempted on multiple entities, but the ``errlist`` is empty, then we
+    can not know which entity caused the error and, thus, ``None`` is used as a name to signify
+    thati fact.
+
+    .. note::
+        Note that the ``errlist`` can contain a special element with a key of "N_MORE_ERRORS".
+        That element means that there were too many errors to place on the ``errlist``.
+        Those errors are suppressed and only their count is provided as a value of the special
+        ``N_MORE_ERRORS`` element.
+    '''
     if ret == 0:
         return
 
