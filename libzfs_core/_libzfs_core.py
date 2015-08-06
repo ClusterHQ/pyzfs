14213809d6a4a38433c276f8e610c7ffda384e79
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-08-06 19:27:47 +0300

    document lzc_list

--- libzfs_core/_libzfs_core.py
+++ libzfs_core/_libzfs_core.py
@@ -797,6 +797,47 @@ lzc_set_prop = lzc_set_props
 
 @_uncommitted()
 def lzc_list(name, options):
+    '''
+    List subordinate elements of the given dataset.
+
+    This function can be used to list child datasets and snapshots
+    of the given dataset.  The listed elements can be filtered by
+    their type and by their depth relative to the starting dataset.
+
+    :param bytes name: the name of the dataset to be listed, could
+                       be a snapshot or a dataset.
+    :param options: a `dict` of the options that control the listing
+                    behavior.
+    :type options: dict of bytes:Any
+    :return: a pair of file descriptors the first of which can be
+             used to read the listing.
+    :rtype: tuple of (int, int)
+
+    Two options are currently available:
+
+    recurse : integer or None
+        specifies depth of the recursive listing. If ``None`` the
+        depth is not limited.
+        Absence of this option means that only the given dataset
+        is listed.
+
+    types : dict of bytes:None
+        specifies dataset types to include into the listing.
+        Currently allowed keys are "filesystem", "volume", "snapshot".
+
+    The first of the returned file descriptors can be used to
+    read the listing in a binary encounded format.  The data is
+    a series of variable sized records each starting with a fixed
+    size header, the header is followed by a serialized ``nvlist``.
+    Each record describes a single element and contains the element's
+    name as well as its properties.
+    The file descriptor must be closed after reading from it.
+
+    The second file descriptor represents a pipe end to which the
+    kernel driver is writing information.  It should not be closed
+    until all interesting information has been read and it must
+    be explicitly closed afterwards.
+    '''
     (rfd, wfd) = os.pipe()
     fcntl.fcntl(rfd, fcntl.F_SETFD, fcntl.FD_CLOEXEC)
     fcntl.fcntl(wfd, fcntl.F_SETFD, fcntl.FD_CLOEXEC)
