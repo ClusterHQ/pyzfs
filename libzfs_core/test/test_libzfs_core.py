2701076f2c8f4e6b5a2c54bddd104dcbb1ab527a
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-08-13 18:12:53 +0300

    add tests for various 'mountpoint' special cases and the normal case

--- libzfs_core/test/test_libzfs_core.py
+++ libzfs_core/test/test_libzfs_core.py
@@ -3272,6 +3272,51 @@ class ZFSTest(unittest.TestCase):
             props = lzc.lzc_get_props(fs)
 
 
+    @unittest.skipUnless(lzc.is_supported(lzc.lzc_get_props), 'not available')
+    def test_get_mountpoint_none(self):
+        fs = ZFSTest.pool.makeName("new")
+        child = ZFSTest.pool.makeName("new/child")
+        props = { "mountpoint": "none" }
+
+        lzc.lzc_create(fs, props = props)
+        lzc.lzc_create(child)
+        actual_props = lzc.lzc_get_props(fs)
+        self.assertDictContainsSubset(props, actual_props)
+        # check that mountpoint value is correctly inherited
+        actual_props = lzc.lzc_get_props(child)
+        self.assertDictContainsSubset(props, actual_props)
+
+
+    @unittest.skipUnless(lzc.is_supported(lzc.lzc_get_props), 'not available')
+    def test_get_mountpoint_legacy(self):
+        fs = ZFSTest.pool.makeName("new")
+        child = ZFSTest.pool.makeName("new/child")
+        props = { "mountpoint": "legacy" }
+
+        lzc.lzc_create(fs, props = props)
+        lzc.lzc_create(child)
+        actual_props = lzc.lzc_get_props(fs)
+        self.assertDictContainsSubset(props, actual_props)
+        # check that mountpoint value is correctly inherited
+        actual_props = lzc.lzc_get_props(child)
+        self.assertDictContainsSubset(props, actual_props)
+
+
+    @unittest.skipUnless(lzc.is_supported(lzc.lzc_get_props), 'not available')
+    def test_get_mountpoint_path(self):
+        fs = ZFSTest.pool.makeName("new")
+        child = ZFSTest.pool.makeName("new/child")
+        props = { "mountpoint": "/mnt" }
+
+        lzc.lzc_create(fs, props = props)
+        lzc.lzc_create(child)
+        actual_props = lzc.lzc_get_props(fs)
+        self.assertDictContainsSubset(props, actual_props)
+        # check that mountpoint value is correctly inherited
+        actual_props = lzc.lzc_get_props(child)
+        self.assertDictContainsSubset({"mountpoint": "/mnt/child"}, actual_props)
+
+
     @unittest.skipUnless(lzc.is_supported(lzc.lzc_rename), 'not available')
     def test_rename(self):
         src = ZFSTest.pool.makeName("source")
