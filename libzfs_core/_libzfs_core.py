6821d02821750e3f13b39bfa6caf2d4e5e50864d
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-09-08 15:49:16 +0300

    _uncommitted: improve the docstring and code self-documentation

--- libzfs_core/_libzfs_core.py
+++ libzfs_core/_libzfs_core.py
@@ -692,33 +692,36 @@ def is_supported(func):
     return getattr(_lib, fname, None) is not None
 
 
-def _uncommitted(check_func = None):
+def _uncommitted(depends_on = None):
     '''
     Mark an API function as being an uncommitted extension that might not be
     available.
 
-    :param function check_func: the function that would be checked
+    :param function depends_on: the function that would be checked
                                 instead of a decorated function.
+                                For example, if the decorated function uses
+                                another uncommitted function.
 
     This decorator transforms a decorated function to raise
     :exc:`NotImplementedError` if the C libzfs_core library does not provide
     a function with the same name as the decorated function.
-    The optional `check_func` parameter can be provided if the decorated function
-    does not directly call the C function but instead calls another Python
-    function that follows the typical convention.
+
+    The optional `depends_on` parameter can be provided if the decorated
+    function does not directly call the C function but instead calls another
+    Python function that follows the typical convention.
     One example is :func:`lzc_list_snaps` that calls :func:`lzc_list` that
     calls ``lzc_list`` in libzfs_core.
 
     This decorator is implemented using :func:`is_supported`.
     '''
-    def _uncommitted_decorator(func, check_func = check_func):
+    def _uncommitted_decorator(func, depends_on = depends_on):
         @functools.wraps(func)
         def _f(*args, **kwargs):
             if not is_supported(_f):
                 raise NotImplementedError(func.__name__)
             return func(*args, **kwargs)
-        if check_func is not None:
-            _f._check_func = check_func
+        if depends_on is not None:
+            _f._check_func = depends_on
         return _f
     return _uncommitted_decorator
 
