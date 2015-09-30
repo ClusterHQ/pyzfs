5c57605643b7b23ca1decf1e9726c1134ef733f5
Andriy Gapon <andriy.gapon@clusterhq.com>
2015-09-30 21:14:58 +0300

    make setup.py pep8 compliant

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "pyzfs",
-    version = "0.2",
-    description = "Wrapper for libzfs_core",
-    author = "ClusterHQ",
-    author_email = "support@clusterhq.com",
-    url = "http://pyzfs.readthedocs.org",
-    license = "Apache License, Version 2.0",
-    classifiers = [
+    name="pyzfs",
+    version="0.2",
+    description="Wrapper for libzfs_core",
+    author="ClusterHQ",
+    author_email="support@clusterhq.com",
+    url="http://pyzfs.readthedocs.org",
+    license="Apache License, Version 2.0",
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
@@ -19,22 +19,22 @@ setup(
         "Topic :: System :: Filesystems",
         "Topic :: Software Development :: Libraries",
     ],
-    keywords = [
+    keywords=[
         "ZFS",
         "OpenZFS",
         "libzfs_core",
     ],
 
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = [
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
         "cffi",
     ],
-    setup_requires = [
+    setup_requires=[
         "cffi",
     ],
-    zip_safe = False,
-    test_suite = "libzfs_core.test",
+    zip_safe=False,
+    test_suite="libzfs_core.test",
 )
 
 # vim: softtabstop=4 tabstop=4 expandtab shiftwidth=4
